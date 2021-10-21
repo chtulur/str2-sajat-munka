4dd73c243ed60f37ec21636d1ebe108c2544aac2
chtulur <molnar.gly@gmail.com>
2021-10-21 16:11:55 +0200

    43

--- extra-feladatok-fetch/script.js
+++ extra-feladatok-fetch/script.js
@@ -1,48 +1,43 @@
 const data = [
-  fetch(
-    "https://raw.githubusercontent.com/jokecamp/FootballData/master/UEFA_European_Championship/Euro%202016/players_json/teams.json"
-  ),
-  fetch(
-    "https://raw.githubusercontent.com/jokecamp/FootballData/master/UEFA_European_Championship/Euro%202016/players_json/hungary-players.json"
-  ),
+  `https://raw.githubusercontent.com/jokecamp/FootballData/master/UEFA_European_Championship/Euro%202016/players_json/teams.json`,
+  `https://raw.githubusercontent.com/jokecamp/FootballData/master/UEFA_European_Championship/Euro%202016/players_json/hungary-players.json`,
 ];
-Promise.all([data[0], data[1]])
+
+Promise.all(data.map((url) => fetch(url)))
   .then((data) => {
     data.forEach((file) => {
       stepIn(file.json());
     });
   })
   .catch((err) => {
-    console.log("ERROR: ", err.message);
+    console.log(`ERROR: `, err.message);
   });
+
 const stepIn = (fetchPromises) => {
   fetchPromises.then((extractedData) => {
-    if (Object.keys(extractedData["sheets"])[0] === "Teams") {
-      hungaryListed(extractedData);
-    } else {
-      playersListed(extractedData);
-    }
+    Object.keys(extractedData[`sheets`])[0] === `Teams`
+      ? printOutTeam(extractedData)
+      : printOutPlayers(extractedData);
   });
 };
-const hungaryListed = (extractedData) => {
-  [0, 1, 2, 4, 6, 7].forEach((info) => {
-    let teamInfo = [Object.entries(extractedData["sheets"]["Teams"][21])[info]];
-    printOutTeam(teamInfo);
+
+const printOutTeam = (extractedData) => {
+  Object.entries(extractedData[`sheets`][`Teams`][21]).forEach(
+    (aspect) =>
+      (document.querySelector(`#team`).innerHTML +=
+        aspect[0] + `: ` + aspect[1] + `<br />`)
+  );
+};
+
+const printOutPlayers = (extractedData) => {
+  extractedData[`sheets`][`Players`].forEach((player) => {
+    document.querySelector(`#player`).innerHTML +=
+      player.name +
+      `, ` +
+      player.position +
+      `, ` +
+      player.club +
+      ` ` +
+      `<br />`;
   });
-};
-const playersListed = (extractedData) => {
-  const guys = [];
-  extractedData["sheets"]["Players"].forEach((player) => {
-    guys.push(
-      player.name + ", " + player.position + ", " + player.club + " " + "<br />"
-    );
-  });
-  printOutPlayers(guys);
-};
-const printOutTeam = (teamInfo) => {
-  document.querySelector("#team").innerHTML +=
-    teamInfo[0][0] + ": " + teamInfo[0][1] + "<br />";
-};
-const printOutPlayers = (guys) => {
-  guys.forEach((g) => (document.querySelector("#player").innerHTML += g));
 };
