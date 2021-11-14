add377c1dc45055905afcb9b59444a1eb7c16fe6
chtulur <molnar.gly@gmail.com>
2021-11-14 13:49:48 +0100

    Done

--- js-2-feladatok-clock/script.js
+++ js-2-feladatok-clock/script.js
@@ -1,57 +1,57 @@
-//Solution II
+// Solution II
 
-// const getLocalTimeInfo = new Intl.DateTimeFormat().resolvedOptions();
-// let nowArr;
+const getLocalTimeInfo = new Intl.DateTimeFormat().resolvedOptions();
+let nowArr;
 
-// const now = (date) => {
-//   nowArr = new Intl.DateTimeFormat(getLocalTimeInfo["locale"], {
-//     localeMatcher: "best fit",
-//     hour12: false,
-//     hour: "numeric",
-//     minute: "numeric",
-//     second: "numeric",
-//   })
-//     .format(date)
-//     .split(":");
-// };
-
-// const assignValues = () => {
-//   document.querySelector("#hours").innerHTML = nowArr[0];
-//   document.querySelector("#minutes").innerHTML = nowArr[1];
-//   document.querySelector("#seconds").innerHTML = nowArr[2];
-// };
-
-// setInterval(() => {
-//   const date = new Date();
-//   now(date);
-//   assignValues();
-// }, 1000);
-
-// const Initdate = new Date();
-// now(Initdate);
-// assignValues();
-
-//Solution I
-
-const theCLockIsTicking = () => {
-  let now = new Date();
-  return (digits = {
-    hours: now.getHours(),
-    minutes: now.getMinutes(),
-    seconds: now.getSeconds(),
-  });
+const now = (date) => {
+  nowArr = new Intl.DateTimeFormat(getLocalTimeInfo["locale"], {
+    localeMatcher: "best fit",
+    hour12: false,
+    hour: "numeric",
+    minute: "numeric",
+    second: "numeric",
+  })
+    .format(date)
+    .split(":");
 };
 
 const assignValues = () => {
-  document.querySelector("#hours").innerHTML = digits.hours;
-  document.querySelector("#minutes").innerHTML = digits.minutes;
-  document.querySelector("#seconds").innerHTML = digits.seconds;
+  document.querySelector("#hours").innerHTML = nowArr[0];
+  document.querySelector("#minutes").innerHTML = nowArr[1];
+  document.querySelector("#seconds").innerHTML = nowArr[2];
 };
 
 setInterval(() => {
-  theCLockIsTicking();
+  const date = new Date();
+  now(date);
   assignValues();
 }, 1000);
 
-theCLockIsTicking();
+const Initdate = new Date();
+now(Initdate);
 assignValues();
+
+//Solution I
+//But no padding for numbers
+// const theCLockIsTicking = () => {
+//   let now = new Date();
+//   return (digits = {
+//     hours: now.getHours(),
+//     minutes: now.getMinutes(),
+//     seconds: now.getSeconds(),
+//   });
+// };
+
+// const assignValues = () => {
+//   document.querySelector("#hours").innerHTML = digits.hours;
+//   document.querySelector("#minutes").innerHTML = digits.minutes;
+//   document.querySelector("#seconds").innerHTML = digits.seconds;
+// };
+
+// setInterval(() => {
+//   theCLockIsTicking();
+//   assignValues();
+// }, 1000);
+
+// theCLockIsTicking();
+// assignValues();
