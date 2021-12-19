ed8a442552ead82af3a821a66a901edaf916ec6a
chtulur <molnar.gly@gmail.com>
2021-12-19 13:38:27 +0100

    Carousel done

--- js-3-feladatok-slider/script.js
+++ js-3-feladatok-slider/script.js
@@ -1,15 +1,56 @@
 let picInFocus = 0;
+let imageCounter = 0;
 
 const container = document.querySelector(".carousel-container");
+const imageContainer = container.querySelector(".image-container");
 const counter = document.querySelector(".counter-number");
+
+const picturesURL = "http://localhost:3000/pictures";
 
 const rightArrow = document.querySelector(".fa-angle-right");
 const leftArrow = document.querySelector(".fa-angle-left");
 
-const images = Array.from(document.querySelectorAll(".image"));
 const circles = Array.from(document.querySelectorAll(".fa-circle"));
+let images;
 
 circles[picInFocus].style.color = "rgb(78, 88, 241)";
+
+const getPictures = async () => {
+  try {
+    return await axios.get(picturesURL).then((response) => response.data);
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+const collectPictures = () => {
+  images = Array.from(document.querySelectorAll(".image"));
+};
+
+const printPics = (picsumURL) => {
+  imageCounter++;
+  let newPic = document.createElement("img");
+  newPic.src = picsumURL;
+  newPic.alt = `carousel-image${imageCounter}`;
+  newPic.classList.add(`img-random${imageCounter}`, "image");
+  if (imageCounter === 1) {
+    newPic.classList.add("showImage");
+  }
+  imageContainer.appendChild(newPic);
+};
+
+const prepareForPrintingPics = () => {
+  getPictures().then((response) => {
+    let pics = response.pop();
+    console.log(pics);
+    for (let pic in pics) {
+      let picsumURL = pics[pic];
+      printPics(picsumURL);
+    }
+    collectPictures();
+  });
+};
+prepareForPrintingPics();
 
 const updateCounter = (picInFocus) => {
   counter.textContent = picInFocus + 1;
@@ -52,8 +93,7 @@ const selectPicture = (ev) => {
   updateCounter(picInFocus);
 };
 
-images[picInFocus].classList.add("showImage");
-setInterval(scrollRight, 3000);
+// setInterval(scrollRight, 3000);
 rightArrow.addEventListener("click", scrollRight);
 leftArrow.addEventListener("click", scrollLeft);
 circles.forEach((circle) => circle.addEventListener("click", selectPicture));
